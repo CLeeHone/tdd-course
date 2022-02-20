b6ebb546a11e2e6a10ce57047be71bd3b4ee228e
Chloe Lee-Hone <chloe.leehone@gmail.com>
2022-02-20 11:40:38 -0500

    create ISBNValidator class and ValidateISBN test class

---
+++ ISBN Validator/test/isbntools/ValidateISBNTest.java
@@ -0,0 +1,10 @@
+package isbntools;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidateISBNTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+}
---
+++ ISBN Validator/test/isbntools/exampleTests.java
@@ -0,0 +1,13 @@
+package isbntools;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class exampleTests {
+
+    @Test
+    public void exampleTest() {
+        //fail();
+    }
+}
