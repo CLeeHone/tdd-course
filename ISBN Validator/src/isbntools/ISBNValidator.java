d977eabd0b9e47399ed9fb778d6fc50c49eae28f
Chloe Lee-Hone <chloe.leehone@gmail.com>
2022-02-21 16:29:55 -0500

    add isOnlyNumeric() method

--- ISBN Validator/src/isbntools/ISBNValidator.java
+++ ISBN Validator/src/isbntools/ISBNValidator.java
@@ -1,8 +1,33 @@
 package isbntools;
 
 public class ISBNValidator {
+    String isbn;
 
     public ISBNValidator() {
     }
 
+    public boolean isValidISBN(String isbn) {
+
+        if (isbn.length() != 10) throw new NumberFormatException("ISBN numbers must be exactly 10 digits long");
+        if (isOnlyNumeric(isbn) == false) throw new NumberFormatException("ISBN numbers must only contain numeric digits [0-9]");
+
+        int sum = 0;
+        int counter = 10;
+
+        for (int i = 0; i < isbn.length(); ++i) {
+            sum += counter * Character.getNumericValue(isbn.charAt(i));
+            --counter;
+        }
+        return (sum % 11 == 0);
+    }
+
+    private boolean isOnlyNumeric(String isbn) {
+
+        for (int i = 0; i <isbn.length(); ++i) {
+            if (!Character.isDigit(isbn.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
