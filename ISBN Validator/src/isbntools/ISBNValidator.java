92304bbe6206848ba36bd872cb8879ad1d14b17c
Chloe Lee-Hone <chloe.leehone@gmail.com>
2022-05-05 15:39:21 -0400

    Refactor isValid13DigitISBN() method and add comments

--- ISBN Validator/src/isbntools/ISBNValidator.java
+++ ISBN Validator/src/isbntools/ISBNValidator.java
@@ -1,32 +1,60 @@
 package isbntools;
 
+/**
+ * The ISBNValidator class determines whether an ISBN is valid or not. ISBNs can be either 10 or 13 digits long.
+ * Logic for ISBN validation logic taken from [1].
+ */
 public class ISBNValidator {
 
+    /**
+     * A short ISBN is exactly {@value} digits long
+     */
     private static final int SHORT_ISBN = 10;
+    /**
+     * A long ISBN is exactly {@value} digits long
+     */
     private static final int LONG_ISBN = 13;
+    /**
+     * A short ISBN has a multiplier of {@value}
+     */
     private static final int SHORT_ISBN_MULTIPLIER = 11;
+    /**
+     * A long ISBN has a multiplier of {@value}
+     */
     private static final int LONG_ISBN_MULTIPLIER = 10;
 
+    /**
+     * Default constructor
+     */
     public ISBNValidator() {
     }
 
+    /**
+     * Determines whether the ISBN is 10 or 13 digits, then validates it. If the ISBN  is neither 10 nor 13 digits
+     * long, it throws a NumberFormatException.
+     * @param isbn The Book's ISBN
+     * @return True if the provided ISBN is valid.
+     */
     public boolean isValidISBN(String isbn) {
-        // If the ISBN is 13 digits
         if (isbn.length() == LONG_ISBN) {
             return isValid13DigitISBN(isbn);
-        } else {
-            // If the ISBN is 10 digits, check ISBN validity
-            if (isbn.length() != SHORT_ISBN) throw new NumberFormatException("ISBN numbers must be exactly 10 digits long, instead of " + isbn.length());
+        }
+
+        if (isbn.length() == SHORT_ISBN) {
             return isValid10DigitISBN(isbn);
         }
+        throw new NumberFormatException("ISBN numbers must be exactly 10 or 13 digits long, instead of " + isbn.length());
     }
 
+    /**
+     * Checks if the provided 13-digit ISBN is valid or not. Used by isValidISBN() method.
+     * @param isbn The Book's ISBN
+     * @return True if the provided 13-digit ISBN is valid
+     */
     public boolean isValid13DigitISBN(String isbn) {
         int thirteenDigitTotal = 0;
-        //int thirteenDigitCounter = 13;
 
         if (isbn.length() == LONG_ISBN) {
-
             for (int i = 0; i < isbn.length(); ++i) {
                 char currentCharacter = isbn.charAt(i);
 
@@ -34,17 +62,17 @@ public class ISBNValidator {
                     return false;
                 }
 
-                if (i % 2 == 0) {
-                    thirteenDigitTotal += Character.getNumericValue(isbn.charAt(i));
-                } else {
-                    thirteenDigitTotal += 3 * Character.getNumericValue(isbn.charAt(i));
-                }
-              //  --thirteenDigitCounter;
+                thirteenDigitTotal += ((i % 2 == 0) ? Character.getNumericValue(isbn.charAt(i)) : (3 * Character.getNumericValue(isbn.charAt(i))));
             }
         }
         return (thirteenDigitTotal % LONG_ISBN_MULTIPLIER == 0);
     }
 
+    /**
+     *  Checks if the provided 10-digit ISBN is valid or not. Used by isValidISBN() method.
+     * @param isbn The Book's isbn
+     * @return True is the provided 10-digit ISBN is valid
+     */
     public boolean isValid10DigitISBN(String isbn) {
         int TenDigitTotal = 0;
         int TenDigitCounter = 10;
@@ -63,4 +91,10 @@ public class ISBNValidator {
         }
         return (TenDigitTotal % SHORT_ISBN_MULTIPLIER == 0);
     }
+
 }
+
+/**
+ * Reference cited:
+ * [1] "ISBN," Wikipedia, Apr. 23, 2022. https://en.wikipedia.org/w/index.php?title=ISBN&oldid=1084316966 (accessed May 03, 2022).
+ */
