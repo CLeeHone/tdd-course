002fe9524e6f61cc2c7224dc12c4919899c2a9f2
Chloe Lee-Hone <chloe.leehone@gmail.com>
2022-05-05 15:37:49 -0400

    Add comments

--- ISBN Validator/src/isbntools/ExternalISBNDataService.java
+++ ISBN Validator/src/isbntools/ExternalISBNDataService.java
@@ -1,6 +1,11 @@
 package isbntools;
 
 public interface ExternalISBNDataService {
+    /**
+     * Used by StockManager and StockManagementTests classes
+     * @param isbn
+     * @return
+     */
     public Book lookup(String isbn);
 }
 
